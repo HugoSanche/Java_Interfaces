98a5f0030b80db9be90618d6cc243bf125f89b90
HugoSanche <hbmclaren@gmail.com>
2023-02-04 16:15:45 -0600

    Initial learning java interfaces

---
+++ repository/AccountRepository.java
@@ -0,0 +1,29 @@
+package repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pojo.Account;
+
+public class AccountRepository {
+
+    private Map<String, Account> datastore = new HashMap<>();
+
+    public void createAccount(Account account) {
+        this.datastore.put(account.getId(), account.clone());
+    }
+
+    public Account retrieveAccount(String id) {
+        return null;
+    }
+
+    public void updateAccount(Account account) {
+
+    }
+
+    public void deleteAccount(String id) {
+
+    }
+
+}
+
